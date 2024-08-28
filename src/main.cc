c9fbb54b88fe6d578543a6354bc51ea3e4a55771
Edgar Brissow <eddiebrissow@gmail.com>
2024-08-28 10:50:54 -0300

    Added more nodes

--- src/main.cc
+++ src/main.cc
@@ -15,16 +15,17 @@
 using namespace attention_broker_server;
 using namespace std;
 
-#define HANDLE_SPACE_SIZE ((unsigned int) 100)
+#define HANDLE_SPACE_SIZE ((unsigned int)100)
 
 using namespace attention_broker_server;
 using namespace std;
 
-
-template<typename T>
-T variance(const std::vector<T> &vec) {
+template <typename T>
+T variance(const std::vector<T> &vec)
+{
     const size_t sz = vec.size();
-    if (sz <= 1) {
+    if (sz <= 1)
+    {
         return 0.0;
     }
 
@@ -32,26 +33,30 @@ T variance(const std::vector<T> &vec) {
     const T mean = std::accumulate(vec.begin(), vec.end(), 0.0) / sz;
 
     // Now calculate the variance
-    auto variance_func = [&mean, &sz](T accumulator, const T& val) {
-        return accumulator + ((val - mean)*(val - mean) / (sz - 1));
+    auto variance_func = [&mean, &sz](T accumulator, const T &val)
+    {
+        return accumulator + ((val - mean) * (val - mean) / (sz - 1));
     };
 
     return std::accumulate(vec.begin(), vec.end(), 0.0, variance_func);
 }
 
-class TestValue: public HandleTrie::TrieValue {
-    public:
-        unsigned int count;
-        TestValue(int count = 1) {
-            this->count = count;
-        }
-        void merge(TrieValue *other) {
+class TestValue : public HandleTrie::TrieValue
+{
+public:
+    unsigned int count;
+    TestValue(int count = 1)
+    {
+        this->count = count;
+    }
+    void merge(TrieValue *other)
+    {
+    }
 
-        }
-
-        std::string to_string(){
-            return std::to_string(this->count);
-        }
+    std::string to_string()
+    {
+        return std::to_string(this->count);
+    }
 };
 
 char R_TLB[16] = {
@@ -73,60 +78,71 @@ char R_TLB[16] = {
     'f',
 };
 
-void benchmark_handletrie(unsigned int n_insertions = 1000000){
+void benchmark_handletrie(unsigned int n_insertions = 1000000)
+{
     char buffer[1000];
     TestValue *value;
-    for (unsigned int key_count: {1, 2, 5}) {
+    for (unsigned int key_count : {1, 2, 5})
+    {
         unsigned int key_size = (HANDLE_HASH_SIZE - 1) * key_count;
         HandleTrie *trie = new HandleTrie(key_size);
-        for (unsigned int i = 0; i < n_insertions; i++) {
-            for (unsigned int j = 0; j < key_size; j++) {
+        for (unsigned int i = 0; i < n_insertions; i++)
+        {
+            for (unsigned int j = 0; j < key_size; j++)
+            {
                 buffer[j] = R_TLB[(rand() % 16)];
             }
             buffer[key_size] = 0;
             string s = buffer;
-            value = (TestValue *) trie->lookup(s);
-            if (value == NULL) {
+            value = (TestValue *)trie->lookup(s);
+            if (value == NULL)
+            {
                 value = new TestValue();
                 trie->insert(s, value);
-            } else {
+            }
+            else
+            {
                 value->count += 1;
             }
         }
-        
     }
 }
 
-
-
-void benchmark_none(unsigned int n_insertions = 1000000){
+void benchmark_none(unsigned int n_insertions = 1000000)
+{
     char buffer[1000];
-    for (unsigned int key_count: {1, 2, 5}) {
+    for (unsigned int key_count : {1, 2, 5})
+    {
         unsigned int key_size = (HANDLE_HASH_SIZE - 1) * key_count;
-        for (unsigned int i = 0; i < n_insertions; i++) {
-            for (unsigned int j = 0; j < key_size; j++) {
+        for (unsigned int i = 0; i < n_insertions; i++)
+        {
+            for (unsigned int j = 0; j < key_size; j++)
+            {
                 buffer[j] = R_TLB[(rand() % 16)];
             }
             buffer[key_size] = 0;
             string s = buffer;
         }
     }
-  
 }
 
-
-void benchmark_map(unsigned int n_insertions = 1000000){
+void benchmark_map(unsigned int n_insertions = 1000000)
+{
     char buffer[1000];
     map<string, int> baseline;
-    for (unsigned int key_count: {1, 2, 5}) {
+    for (unsigned int key_count : {1, 2, 5})
+    {
         unsigned int key_size = (HANDLE_HASH_SIZE - 1) * key_count;
-        for (unsigned int i = 0; i < n_insertions; i++) {
-            for (unsigned int j = 0; j < key_size; j++) {
+        for (unsigned int i = 0; i < n_insertions; i++)
+        {
+            for (unsigned int j = 0; j < key_size; j++)
+            {
                 buffer[j] = R_TLB[(rand() % 16)];
             }
             buffer[key_size] = 0;
             string s = buffer;
-            if (baseline.find(s) == baseline.end()) {
+            if (baseline.find(s) == baseline.end())
+            {
                 baseline[s] = 0;
             }
             baseline[s] = baseline[s] + 1;
@@ -134,18 +150,20 @@ void benchmark_map(unsigned int n_insertions = 1000000){
     }
 }
 
-
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[])
+{
 
     string option = argv[1];
-    if (option == "handletrie"){
+    if (option == "handletrie")
+    {
         benchmark_handletrie(atoi(argv[2]));
     }
-    if (option == "none"){
+    if (option == "none")
+    {
         benchmark_none(atoi(argv[2]));
     }
-    if (option == "map"){
+    if (option == "map")
+    {
         benchmark_map(atoi(argv[2]));
     }
 
